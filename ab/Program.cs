803f2c69c91d563db22cd13216a7af261a60caf5
Alexander <bellal89@mail.ru>
2013-01-29 23:04:54 +0600

    1044: "Lucky Tickets. Easy!" accepted.
    http://ega-math.narod.ru/Quant/Tickets.htm

--- ab/Program.cs
+++ ab/Program.cs
@@ -1,45 +1,37 @@
 ﻿using System;
-using System.Diagnostics;
 
 namespace ab
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			var sw = new Stopwatch();
-			for (var j = 2; j < 9; j += 2)
-			{
-				sw.Start();
-				Console.WriteLine(j + ":\t" + GetLucky(j));
-				sw.Stop();
-				Console.WriteLine(sw.Elapsed);
-				sw.Reset();
-			}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetLuckysAmount(j / 2));
+        }
 
-			Console.ReadKey();
-		}
-		
-		private static int GetLucky(int num)
-		{
-			var maxNum = ((int)Math.Pow(10, num)) - 1;
-			var res = 0;
-			for (var j = 0; j <= maxNum; j++)
-			{
-				var half1 = 0;
-				var half2 = 0;
-				for (var i = 0; i < num; i++)
-				{
-					var tmp = (j / ((int)Math.Pow(10, i))) % 10;
-					if (i < num / 2)
-						half1 += tmp;
-					else
-						half2 += tmp;
-				}
-				if (half1 == half2)
-					res++;
-			}
-			return res;
-		}
-	}
+        private static int GetLuckysAmount(int digitsAmount)
+        {
+            int amount = 0;
+            for (int i = 0; i <= 9 * digitsAmount; i++)
+            {
+                int tmp = NAmountWithKSum(digitsAmount, i);
+                amount += tmp * tmp;
+            }
+            return amount;
+        }
+
+        private static int NAmountWithKSum(int n, int k)
+        {
+            if (n == 1)
+                return 0 <= k && k <= 9 ? 1 : 0;
+            int sum = 0;
+            for (int i = 0; i <= 9; i++)
+            {
+                if (i <= k)
+                    sum += NAmountWithKSum(n - 1, k - i);
+            }
+            return sum;
+        }
+    }
 }
